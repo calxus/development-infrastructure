63d8414a7ccab709042c83e25fbf75fbd171f716
Gordon Adam <gtadam@protonmail.ch>
2020-06-25 23:00:53 +0100

    Add initial VPC deployment

---
+++ main.tf
@@ -0,0 +1,28 @@
+data "aws_availability_zones" "current" {}
+
+resource "aws_vpc" "development" {
+  cidr_block = var.vpc_cidr_block
+}
+
+resource "aws_security_group" "development" {
+  name        = var.name
+  description = "Rules to allow SSH access"
+  vpc_id      = aws_vpc.development.id
+}
+
+resource "aws_security_group_rule" "allow_ingress_ssh" {
+  type              = "ingress"
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = 22
+  security_group_id = aws_security_group.development.id
+}
+
+resource "aws_subnet" "development" {
+  count                   = length(data.aws_availability_zones.current.zone_ids)
+  cidr_block              = cidrsubnet(var.vpc_cidr_block, var.subnets.public.newbits, var.subnets.public.netnum + count.index)
+  vpc_id                  = aws_vpc.development.id
+  availability_zone_id    = data.aws_availability_zones.current.zone_ids[count.index]
+  map_public_ip_on_launch = true
+}
--- variables.tf
+++ variables.tf
@@ -3,6 +3,25 @@ variable "name" {
   default = "development-infrastructure"
 }
 
+variable "vpc_cidr_block" {
+  type = string
+}
+
 variable "region" {
   type = string
+}
+
+variable "subnets" {
+  description = "define sizes for subnets using Terraform cidrsubnet function. For an empty /24 VPC, the defaults will create /28 public subnets and /26 private subnets, one of each in each AZ."
+  type        = map(map(number))
+  default = {
+    public = {
+      newbits = 4
+      netnum  = 0
+    }
+    private = {
+      newbits = 2
+      netnum  = 1
+    }
+  }
 }
